345e9f25c64843551e37582e7527dfbbb5f9ab07
Lyle Scott III <lyle@digitalfoo.net>
2012-06-30 02:27:04 -0400

    Use proper UTF symbols for suit.

--- src/deck.py
+++ src/deck.py
@@ -9,18 +9,10 @@ class Deck(object):
     linebreak_index = 5
 
     # Map the suit to a textual representation.
-    suits = {'heart': 
-                 {'black': u'\u2661',
-                  'red': u'\u2665'},
-             'diamond':
-                 {'black': u'\u2666',
-                  'red': u'\u2662'},
-             'club':
-                 {'black': u'\u2663',
-                  'red': u'\u2667'},
-             'spade':
-                 {'black': u'\u2660',
-                  'red': u'\u2664'},}
+    suits = {'heart':   u'\u2665',
+             'diamond': u'\u2666',
+             'club':    u'\u2667',
+             'spade':   u'\u2664',}
 
 
     def __init__(self, name='Deck', cards=None):
@@ -33,13 +25,12 @@ class Deck(object):
     def generate_deck(self, shuffled=True):
         """Generate a deck of 52 unshuffled cards."""
         self.cards = []
-        for colorcodes in self.suits.values():
-            for color in colorcodes:
-                for value in xrange(1, 14):
-                    card = Card(colorcodes[color], value)
-                    self.cards.append(card)
-                    if shuffled:
-                        self.shuffle_deck()
+        for suit in self.suits.values():
+            for value in xrange(1, 14):
+                card = Card(suit, value)
+                self.cards.append(card)
+                if shuffled:
+                    self.shuffle_deck()
 
     def shuffle_deck(self):
         """Shuffle the deck in place."""
