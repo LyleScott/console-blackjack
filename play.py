6f138f099575f0330227f7e5f149414e127e939e
Lyle Scott III <lyle@digitalfoo.net>
2012-07-20 21:22:07 -0400

    Require python3.

--- play.py
+++ play.py
@@ -1,14 +1,28 @@
 
+import platform
+import sys
+
 from src.board import Board
 
 N_PLAYERS = 3
 N_DECKS = 4
 
+def check_version():
+    """Python 3 required to run this!"""
+    pyversion = platform.python_version()
+    
+    v = int(pyversion[0])
+    
+    if v < 3:
+        print('You need Python 3.x to run this.')
+        sys.exit(-2)
+
 def main():
+    """Let's get it on..."""
     board = Board(n_players=N_PLAYERS, n_decks=N_DECKS)
     board.play()
 
 
 if __name__ == '__main__':
+    check_version()
     main()
-
